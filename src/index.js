073a1979a1b6c5cb2ea6fd0e20ff3e388238d98f
Oli Zimpasser <oglimmer@gmail.com>
2022-12-03 17:24:19 +0100

    Fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const axios = require('axios')
+const decompress = require('brotli/decompress')
 
-axios.get('https://meme-api.com/gimme/ITMemes').then(response => {
-  console.log(response.data) // this should be JSON
+axios.get('https://meme-api.com/gimme/ITMemes', {
+  responseType: 'arraybuffer'
+}).then(response => {
+  const encodedArray = decompress(response.data)
+  const decodedString = new TextDecoder().decode(encodedArray);
+  const json = JSON.parse(decodedString)
+  console.log(json)
 });
