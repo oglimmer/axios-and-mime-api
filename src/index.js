91189f2555b8ea17ae038fb5a70df393997b8e49
Oli Zimpasser <oglimmer@gmail.com>
2022-12-03 17:51:56 +0100

    Use stream instead of arraybuffer. Removes dependency to brotli lib and uses module zlib instead

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const axios = require('axios')
-const decompress = require('brotli/decompress')
+const zlib = require('zlib')
+
+function streamToString (stream) {
+  const chunks = [];
+  return new Promise((resolve, reject) => {
+    stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+    stream.on('error', (err) => reject(err));
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+  })
+}
 
 axios.get('https://meme-api.com/gimme/ITMemes', {
-  responseType: 'arraybuffer'
-}).then(response => {
-  const encodedArray = decompress(response.data)
-  const decodedString = new TextDecoder().decode(encodedArray);
-  const json = JSON.parse(decodedString)
-  console.log(json)
+  decompress: false,
+  responseType: 'stream',
+  transformResponse: (data) => data.pipe(zlib.createBrotliDecompress())
+})
+.then(response => streamToString(response.data))
+.then(data => {
+  console.log(data)
 });
